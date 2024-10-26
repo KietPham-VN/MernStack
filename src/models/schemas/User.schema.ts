f5f40eac3ae69cdfff2ec088bb72fd9109ef4ea1
Phạm Anh Kiệt <phamanhkiet.dev@gmail.com>
2024-10-26 10:16:57 +0700

    update operator

--- src/models/schemas/User.schema.ts
+++ src/models/schemas/User.schema.ts
@@ -56,22 +56,22 @@ export default class User {
   constructor(user: UserType) {
     const date = new Date() //tạo này cho ngày created_at updated_at bằng nhau
     this._id = user._id || new ObjectId() // tự tạo id
-    this.name = user.name || '' // nếu người dùng tạo mà k truyền ta sẽ để rỗng
+    this.name = user.name ?? '' // nếu người dùng tạo mà k truyền ta sẽ để rỗng
     this.email = user.email
     this.date_of_birth = user.date_of_birth || date
     this.password = user.password
     this.created_at = user.created_at || date
     this.updated_at = user.updated_at || date
-    this.email_verify_token = user.email_verify_token || ''
-    this.forgot_password_token = user.forgot_password_token || ''
-    this.verify = user.verify || UserVerifyStatus.Unverified
+    this.email_verify_token = user.email_verify_token ?? ''
+    this.forgot_password_token = user.forgot_password_token ?? ''
+    this.verify = user.verify ?? UserVerifyStatus.Unverified
 
-    this.bio = user.bio || ''
-    this.location = user.location || ''
-    this.website = user.website || ''
-    this.username = user.username || ''
-    this.avatar = user.avatar || ''
-    this.cover_photo = user.cover_photo || ''
-    this.role = user.role || USER_ROLE.User
+    this.bio = user.bio ?? ''
+    this.location = user.location ?? ''
+    this.website = user.website ?? ''
+    this.username = user.username ?? ''
+    this.avatar = user.avatar ?? ''
+    this.cover_photo = user.cover_photo ?? ''
+    this.role = user.role ?? USER_ROLE.User
   }
 }
