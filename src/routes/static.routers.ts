0a7b53a0d0fca362fbba692178d92c15b7b5e3d8
Phạm Anh Kiệt <phamanhkiet.dev@gmail.com>
2024-11-17 03:08:10 +0700

    last lesson from Diep sensei

--- src/routes/static.routers.ts
+++ src/routes/static.routers.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { serveImageController } from '~/controllers/static.controllers'
+import { serveImageController, serveVideoController } from '~/controllers/static.controllers'
 const staticRouter = Router()
 
 // staticRouter.use('/image', express.static(UPLOAD_IMAGE_DIR))
 staticRouter.get('/image/:namefile', serveImageController)
-
+staticRouter.get('/video/:namefile', serveVideoController)
+// staticRouter.use('/video', express.static(UPLOAD_VIDEO_DIR))
 export default staticRouter
